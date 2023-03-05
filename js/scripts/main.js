91537ce679864c06b0619eb5483299cdba29c213
ruivergani <ruiverganineto@gmail.com>
2023-03-05 21:26:58 +0000

    Swiper slide pagination almost done

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -16,3 +16,32 @@ if(menuButton){
         document.documentElement.classList.toggle('menu-opened');
     })
 }
+
+// Menu Header Change >20 scroll
+const header = document.getElementById("js-header");
+function fixedMenu() {
+  if (window.pageYOffset > 50) {
+    header.classList.add("changeHeight");
+  } else {
+    header.classList.remove("changeHeight");
+  }
+}
+document.addEventListener("scroll", fixedMenu);
+
+
+// Script for Swipper Slide
+var swiper = new Swiper(".slide-principal", {
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false // autoplay nao para de funcionar
+    },
+    effect: 'fade',
+    pagination: {
+      el: ".slide-principal .swiper-pagination",
+      clickable: true,
+      type: "fraction",
+    },
+    breakpoints:{
+
+    }
+});
