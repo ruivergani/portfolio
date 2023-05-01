8b2b1fb86b0aa222cc91b314d8af96a45e871bb1
ruivergani <ruiverganineto@gmail.com>
2023-05-01 18:19:34 +0100

    changes js file

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,7 +1,5 @@
 // Script Data AOS Animation
-AOS.init({
-  disable: "mobile"
-});
+AOS.init();
 
 // Button Scroll Top
 const btnScrollTop = document.getElementById("js-btn-scroll-top");
@@ -41,81 +39,80 @@ function fixedMenu() {
 document.addEventListener("scroll", fixedMenu);
 
 const progressSlide = document.querySelector('.js-progress'); // get progress bar
-
-// Script for Cases Slide
-"use strict";
-  var slide_image_cases = new Swiper(".slide-image-cases",{
-      slidesPerView: 'auto',
-      spaceBetween: 32,
-      loop: !0,
-      speed: 800,
-      centeredSlides: !0,
-      watchSlidesVisibility: !0,
-      watchSlidesProgress: !0,
-      pagination: {
-        el: ".s-cases .ctrl-slide .pagination-box-cases",
-      },
-      navigation: {
-        nextEl: ".s-cases .ctrl-slide .btn-next",
-        prevEl: ".s-cases .ctrl-slide .btn-prev"
-      },
-  }),
-  slide_about_case = new Swiper(".slide-about-case",{
-      spaceBetween: 40,
-      direction: "vertical",
-      speed: 800,
-      pagination: {
-        el: ".s-cases .ctrl-slide .page-fraction",
-        type: "fraction",
-        clickable: false,
-      },
-      navigation: {
-        nextEl: ".s-cases .ctrl-slide .btn-next",
-        prevEl: ".s-cases .ctrl-slide .btn-prev"
-      },
-      simulateTouch: !1,
-      thumbs: {
-        swiper: slide_image_cases,
-      },
-  }),
-  // Script for Hero Slide
-  swiper = new Swiper(".slide-principal", {
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false // autoplay nao para de funcionar
-    },
-    effect: 'fade',
-    pagination: {
-      el: ".slide-principal .swiper-pagination",
-      clickable: true,
-      type: "fraction",
-    },
-    on: { // get some elements from swiper slide
-        init : function () { // when slide starts
-          progressSlide.classList.remove('animate');
-          progressSlide.classList.remove('active');
-          progressSlide.classList.add('animate'); // faz a barra crescer
-          progressSlide.classList.add('active'); // da opacidade
-        }, 
-        slideChangeTransitionStart : function (){ // when the transition start
-          progressSlide.classList.remove('animate');
-          progressSlide.classList.remove('active');
-          progressSlide.classList.add('active');
-        }, 
-        slideChangeTransitionEnd : function (){ // when the transiction finish
-          progressSlide.classList.add('animate'); // um apos o outro - nao pode adicionar junto
-        }
+// Script for Hero Slide
+var swiper = new Swiper(".slide-principal", {
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false // autoplay nao para de funcionar
+  },
+  effect: 'fade',
+  pagination: {
+    el: ".slide-principal .swiper-pagination",
+    clickable: true,
+    type: "fraction",
+  },
+  on: { // get some elements from swiper slide
+      init : function () { // when slide starts
+        progressSlide.classList.remove('animate');
+        progressSlide.classList.remove('active');
+        progressSlide.classList.add('animate'); // faz a barra crescer
+        progressSlide.classList.add('active'); // da opacidade
+      }, 
+      slideChangeTransitionStart : function (){ // when the transition start
+        progressSlide.classList.remove('animate');
+        progressSlide.classList.remove('active');
+        progressSlide.classList.add('active');
+      }, 
+      slideChangeTransitionEnd : function (){ // when the transiction finish
+        progressSlide.classList.add('animate'); // um apos o outro - nao pode adicionar junto
       }
-  }),
-  // Script for Mobile Navigation
-  slide_mobile_case = new Swiper(".slide-cases-mobile", {
-      slidesPerView: 'auto',
-      pagination: {
-        el: ".s-cases .container .slide-cases-mobile .swiper-pagination",
-        type: "progressbar",
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-  });
+    }
+});
+// Script for Slide Image Cases
+var slide_image_cases = new Swiper(".slide-image-cases",{
+  slidesPerView: 'auto',
+  spaceBetween: 32,
+  loop: !0,
+  speed: 800,
+  centeredSlides: !0,
+  watchSlidesVisibility: !0,
+  watchSlidesProgress: !0,
+  pagination: {
+    el: ".s-cases .ctrl-slide .pagination-box-cases",
+  },
+  navigation: {
+    nextEl: ".s-cases .ctrl-slide .btn-next",
+    prevEl: ".s-cases .ctrl-slide .btn-prev"
+  },
+});
+// Script for Slide About Cases
+var slide_about_case = new Swiper(".slide-about-case",{
+  spaceBetween: 40,
+  direction: "vertical",
+  speed: 800,
+  pagination: {
+    el: ".s-cases .ctrl-slide .page-fraction",
+    type: "fraction",
+    clickable: false,
+  },
+  navigation: {
+    nextEl: ".s-cases .ctrl-slide .btn-next",
+    prevEl: ".s-cases .ctrl-slide .btn-prev"
+  },
+  simulateTouch: !1,
+  thumbs: {
+    swiper: slide_image_cases,
+  },
+});
+// Script for Mobile Navigation
+var slide_mobile_case = new Swiper(".slide-cases-mobile", {
+  slidesPerView: 'auto',
+  pagination: {
+    el: ".s-cases .container .slide-cases-mobile .swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
